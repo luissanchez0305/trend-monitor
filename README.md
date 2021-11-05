1ad90685aa4c77451b64921112d93a5c0c51e9cc
Jignesh Ahir <jigu015@gmail.com>
2021-11-05 07:59:06 -0700

    Vuejs charts project commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# trend-monitor
+<<<<<<< HEAD
+=======
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
+>>>>>>> b1a8c28 (Trading Chart en proyecto Vuejs)
---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <!--<img alt="Vue logo" src="./assets/logo.png">-->
+  <!--<HelloWorld msg="Welcome to Your Vue.js App"/>-->
+  <Charts msg="Trading Chart in VueJS" />
+</template>
+
+<script>
+import Charts from './components/Charts.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Charts,
+  }
+}
+</script>
+
+<style>
+body {
+  background-color: aquamarine;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 0 auto;
+  background-color: white;
+}
+</style>
